6e86e34fbdb76097a6e496788d0df39070e0f3b0
Dima <semm0202@yandex.ru>
2020-10-11 18:53:49 +0300

    patch 1.000000005 unstable touch handler

--- core/src/com/nwp/game/Game.java
+++ core/src/com/nwp/game/Game.java
@@ -3,28 +3,27 @@ package com.nwp.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputAdapter;
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
-import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.Array;
-import com.badlogic.gdx.utils.TimeUtils;
 import com.nwp.game.objects.Ball;
+import com.nwp.game.objects.TouchAdapter;
 import com.nwp.game.source.Gutter;
 
 public class Game extends ApplicationAdapter {
-	static Ball ShootBall;
 	SpriteBatch batch;
 	Array<Texture> textures;
 	private Gutter test_gutter;
 	static Texture second_ball;
+	static Sprite background;
+	private TouchAdapter test_adapter;
 	OrthographicCamera camera;
+	private Array<Integer> y_levels;
 	int touch_x;
 	int touch_y;
 	float height;
@@ -45,11 +44,18 @@ public class Game extends ApplicationAdapter {
 		camera = new OrthographicCamera(width,height);// устанавливаем переменные высоты и ширины в качестве области просмотра нашей игры
 		camera.setToOrtho(false);// этим методом мы центруем камеру на половину высоты и половину ширины
 		batch = new SpriteBatch();
+		background = new Sprite(new Texture(Gdx.files.internal("gutter.png")));
+		background.setSize(background.getWidth(), background.getHeight());
+		background.setPosition(0,950);
+		y_levels = new Array<>();
+		y_levels.add(1000);
 		textures = new Array<>();
 		textures.add(new Texture(Gdx.files.internal("ball.png")));
 		textures.add(new Texture(Gdx.files.internal("ball_1.png")));
 		textures.add(new Texture(Gdx.files.internal("ball_2.png")));
 		test_gutter = new Gutter(1000,2.5f,width,textures);
+		test_adapter = new TouchAdapter(textures,500,30,100,-10);
+
 	}
 
 	@Override
@@ -57,143 +63,27 @@ public class Game extends ApplicationAdapter {
 		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		test_gutter.render_gutter(batch,ShootBall);
-		if (ShootBall!= null && ShootBall.position.x==0)
-			ShootBall = null;
-//		for(Ball ball: balls) {
-//			if (is_move_balls){
-//				ball.set_velocity();
-//				ball.update();
-//			}
-//			else
-//				if (pos_collapse!=-1 && balls.indexOf(ball,false)<pos_collapse){
-//					if (ball.position.x == cord_collapse.x) {
-//						System.out.println(String.valueOf(balls.indexOf(ball, false)) + " " + String.valueOf(pos_collapse));
-////						Ball new_ball;
-////						new_ball= balls.get(pos_collapse+1);
-////						if (new_ball.ballSprite.getTexture().toString().equals(ball.ballSprite.getTexture().toString())){
-////							int left = pos_collapse;
-////							int right = pos_collapse;
-////							do{
-////								left--;
-////								new_ball= balls.get(left);
-////							}while (left > 0 && new_ball.ballSprite.getTexture().toString().equals(ball.ballSprite.getTexture().toString()));
-////							do{
-////								right++;
-////								new_ball= balls.get(right);
-////							}while  (new_ball.ballSprite.getTexture().toString().equals(ball.ballSprite.getTexture().toString()));
-////							if (right-left>=3){
-////								pos_collapse = left+1;
-////								cord_collapse = new Vector2(balls.get(right-1).position);
-////								balls.removeRange(left+1,right-1);
-////							}
-////							else{
-////								is_move_balls = true;
-////								cord_collapse = null;
-////								pos_collapse = - 1;
-////							}
-////						}
-////						else{
-////
-////							cord_collapse = null;
-////							pos_collapse = - 1;
-////						}
-////					}
-//						is_move_balls = true;
-//						ball.update();
-//					}
-//				}
-//			ball.render(batch);
-//
-////					new_ball = balls.get(ball_pos-1);
-////					if (new_ball.ballSprite.getTexture().toString().equals(shoot_tex)){
-////						new_ball = balls.get(ball_pos-2);
-////						if (new_ball.ballSprite.getTexture().toString().equals(shoot_tex)){
-////							pos_collapse = ball_pos-3;
-////							cord_collapse = new Vector2(balls.get(ball_pos).position);
-////							balls.removeRange(ball_pos-2,ball_pos);
-////						}
-////						else {
-////							new_ball = balls.get(ball_pos+1);
-////							if (new_ball.ballSprite.getTexture().toString().equals(shoot_tex)){
-////								pos_collapse = ball_pos-2;
-////								cord_collapse = new Vector2(balls.get(ball_pos+1).position);
-////								balls.removeRange(ball_pos-1,ball_pos+1);
-////							}
-////						}
-////					}
-////					else {
-////						new_ball = balls.get(ball_pos+1);
-////						if (new_ball.ballSprite.getTexture().toString().equals(shoot_tex)){
-////							new_ball = balls.get(ball_pos+2);
-////							if (new_ball.ballSprite.getTexture().toString().equals(shoot_tex)){
-////								pos_collapse = ball_pos-1;
-////								cord_collapse = new Vector2(balls.get(ball_pos+2).position);
-////								balls.removeRange(ball_pos,ball_pos+2);
-////							}
-////						}
-////					}
-////					if (pos_collapse!=-1){
-////						pos_collapse++;
-////						for(int i = pos_collapse; i>=0;i--){
-////							Vector2 speed = new Vector2(balls.get(i).velocity);
-////							balls.get(i).velocity.set(speed.rotate(180).scl(1.5f,1.5f));
-////						}
-////					}
-////					else
-////						is_move_balls = true;
-//				}
-//			}
-//		}
-		if (ShootBall!=null){
-			ShootBall.update();
-			ShootBall.render(batch);
+		background.draw(batch);
+		test_adapter.update_counters();
+		test_gutter.render_gutter(batch,test_adapter.get_shoot_ball());
+		if (test_adapter.is_ball_shooting() && test_adapter.get_shoot_ball().position.x==0)
+			test_adapter.delete_ball();
+		if (test_adapter.is_ball_shooting()){
+			test_adapter.get_shoot_ball().update();
+			test_adapter.get_shoot_ball().render(batch);
 		}
 		batch.end();
-		if (Gdx.input.isTouched()) {
-			if (ShootBall!=null) {
-				Vector2 center = new Vector2(ShootBall.position.x+ShootBall.ballSprite.getWidth()/2, ShootBall.position.y+ShootBall.ballSprite.getHeight()/2);
-				Vector2 point1 = new Vector2(touch_x,touch_y);
-				Vector2 point2 = new Vector2(center.x + 1, center.y);
-				point1.sub(center).nor();
-				point2.sub(center).nor();
-				float angle = (MathUtils.atan2(point1.y, point1.x) - MathUtils.atan2(point2.y, point2.x));
-				Vector2 point3 = new Vector2(center.x + 300 * MathUtils.cos(MathUtils.PI - angle), center.y - 300 * MathUtils.sin(MathUtils.PI - angle));
-				ShapeRenderer sr = new ShapeRenderer();
-				sr.setColor(Color.BLACK);
-				sr.begin(ShapeRenderer.ShapeType.Filled);
-				sr.rectLine(center.x, center.y, point3.x, point3.y, 10);
-				sr.end();
 
-			}
-
-		}
 		Gdx.input.setInputProcessor(new InputAdapter(){
 			@Override
 			public boolean touchDown (int x, int y, int pointer, int button) {
 				Vector3 touchPos = new Vector3();
 				touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
 				camera.unproject(touchPos);
-				ShootBall = new Ball(new Texture(Gdx.files.internal("ball_1.png")),new Vector2(touchPos.x, touchPos.y));
-				return false;
-			}
-			@Override
-			public boolean touchUp (int x, int y, int pointer, int button) {
-				Vector2 center = new Vector2(ShootBall.position.x+ShootBall.ballSprite.getWidth()/2, ShootBall.position.y+ShootBall.ballSprite.getHeight()/2);
-				Vector2 dir = new Vector2();
-				Vector2 touch = new Vector2(touch_x,touch_y);
-				dir.set(touch).sub(center).nor();
-				ShootBall.velocity.set(dir.rotate(180).scl(12,12));
-				return false;
-			}
-			@Override
-			public boolean touchDragged (int x, int y, int pointer) {
-				touch_x = x;
-				touch_y = (int)height - y;
+				test_adapter.handle_touch(new Vector2(touchPos.x, touchPos.y), y_levels);
 				return false;
 			}
 		});
-
 	}
 	
 	@Override
---
+++ core/src/com/nwp/game/objects/TouchAdapter.java
@@ -0,0 +1,130 @@
+package com.nwp.game.objects;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.Array;
+import com.nwp.game.Game;
+
+public class TouchAdapter {
+    private Array<Texture> textures;
+
+    // pseudo random
+    private Array<Integer> pseudo_random_indexes;
+    private int pseudo_random_iterator = 0;
+    private int pseudo_random_size_selection = 5;
+
+    private int default_touch_area;
+    private int forbidden_touch_area;
+    private int default_touch_delay;
+    private int touch_delay = 0;
+    private float down_speed;
+    private Ball shoot_ball;
+
+    public TouchAdapter(Array<Texture> textures,
+                        int default_touch_area,
+                        int forbidden_touch_area,
+                        int default_touch_delay,
+                        float down_speed) {
+        this.textures = textures;
+        this.default_touch_area = default_touch_area;
+        this.forbidden_touch_area = forbidden_touch_area;
+        this.default_touch_delay = default_touch_delay;
+        this.down_speed = down_speed;
+        this.pseudo_random_indexes = new Array<>();
+        set_pseudo_random_textures();
+    }
+
+    // shoot ball methods
+    public Ball get_shoot_ball() {
+        return shoot_ball;
+    }
+
+    public boolean is_ball_shooting() {
+        return shoot_ball != null;
+    }
+
+    private void spawn_shoot_ball(Vector2 touch_position) {
+        Texture ball_texture = get_random_ball_texture();
+        shoot_ball = new Ball(ball_texture,new Vector2(touch_position));
+        shoot_ball.velocity = new Vector2(0,down_speed);
+    }
+
+    public void delete_ball(){
+        shoot_ball = null;
+    }
+
+    // delay methods
+    private void set_touch_delay(int delay) {
+        touch_delay = delay;
+    }
+
+    private void set_touch_delay() {
+        touch_delay = default_touch_delay;
+    }
+
+    private boolean check_delay() {
+        if (touch_delay != 0) {
+            touch_delay--;
+            return touch_delay == 0;
+        }
+        return true;
+    }
+
+    public void update_counters() {
+        //maybe something there or not
+        if (touch_delay!=0)
+            touch_delay--;
+    }
+
+    // random texture method
+    private Texture get_random_ball_texture() {
+        pseudo_random_iterator++;
+        if (pseudo_random_iterator == pseudo_random_indexes.size - 1) {
+            pseudo_random_iterator = 0;
+            set_pseudo_random_textures();
+        }
+        return textures.get(pseudo_random_indexes.get(pseudo_random_iterator));
+    }
+
+    private void set_pseudo_random_textures() {
+        pseudo_random_indexes = new Array<>();
+        for (int i = 0; i < pseudo_random_size_selection; i++) {
+            pseudo_random_indexes.add(Game.get_rand_in_range(0, textures.size - 1));
+        }
+        int mid_selection_index = pseudo_random_size_selection / 2;
+        if (pseudo_random_indexes.get(mid_selection_index).equals(pseudo_random_indexes.get(mid_selection_index - 1))
+                || pseudo_random_indexes.get(mid_selection_index).equals(pseudo_random_indexes.get(mid_selection_index + 1))) {
+            int num_matches = 1;
+            if (pseudo_random_indexes.get(mid_selection_index).equals(pseudo_random_indexes.get(mid_selection_index - 1))) {
+                for (int i = mid_selection_index; i > 0; i--)
+                    if (pseudo_random_indexes.get(i).equals(pseudo_random_indexes.get(i - 1)))
+                        num_matches++;
+
+            } else {
+                for (int i = mid_selection_index; i < pseudo_random_indexes.size - 1; i++) {
+                    if (pseudo_random_indexes.get(i).equals(pseudo_random_indexes.get(i + 1)))
+                        num_matches++;
+                }
+            }
+            if (num_matches == mid_selection_index)
+                if (pseudo_random_indexes.get(mid_selection_index) == 0 || pseudo_random_indexes.get(mid_selection_index) != textures.size - 1)
+                    pseudo_random_indexes.set(mid_selection_index, pseudo_random_indexes.get(mid_selection_index) + 1);
+                else
+                    pseudo_random_indexes.set(mid_selection_index, pseudo_random_indexes.get(mid_selection_index) - 1);
+        }
+    }
+
+    public void handle_touch(Vector2 touch_position, Array<Integer> all_y_levels) {
+        if (check_delay()) {
+            boolean possibility_shoot = false;
+            for (int level : all_y_levels)
+                if (level - forbidden_touch_area <= touch_position.y && level + default_touch_area >= touch_position.y) {
+                    possibility_shoot = true;
+                    break;
+                }
+            if (possibility_shoot)
+                spawn_shoot_ball(touch_position);
+            set_touch_delay();
+        }
+    }
+}
